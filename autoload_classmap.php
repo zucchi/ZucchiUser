babefdfcf9e4bc8af5f68dab1a44b5cc50eecd3f
Matt Cockayne <matt@zucchi.co.uk>
2012-08-26 19:23:06 +0100

    updated classmap

--- autoload_classmap.php
+++ autoload_classmap.php
@@ -2,4 +2,6 @@
 return array(
     'ZucchiUser\Module'                        => __DIR__ . '/Module.php',
     'ZucchiUser\Controller\AdminController'      => __DIR__ . '/src/ZucchiUser/Controller/AdminController.php',
+    'ZucchiUser\Entity\User'      => __DIR__ . '/src/ZucchiUser/Entity/user.php',
+    'ZucchiUser\Service\User'      => __DIR__ . '/src/ZucchiUser/Service/user.php',
 );
