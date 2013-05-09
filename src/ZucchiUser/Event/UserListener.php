4d58a3b44a156cc0bfd4392948ba17298ab94e70
Richard Nicol <richardnicol71@gmail.com>
2013-05-09 10:25:11 +0100

    Update copyright.

--- src/ZucchiUser/Event/UserListener.php
+++ src/ZucchiUser/Event/UserListener.php
@@ -3,7 +3,7 @@
  * ZucchiSecurity (http://zucchi.co.uk/)
  *
  * @link      http://github.com/zucchi/ZucchiSecurity for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zucchi Limited (http://zucchi.co.uk)
+ * @copyright Copyright (c) 2005-2013 Zucchi Limited (http://zucchi.co.uk)
  * @license   http://zucchi.co.uk/legals/bsd-license New BSD License
  */
 namespace ZucchiUser\Event;
